a4fddf8520e501b5020a86322c82d1ad9967619e
GIOVANNI GUSTAVO SANTOS SOUSA <giovanni_sousa@estudante.sesisenai.org.br>
2025-04-22 15:39:45 -0300

    atualzando

--- lib/screens/todo_list_page.dart
+++ lib/screens/todo_list_page.dart
@@ -11,23 +11,92 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Row(
+
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'adicione uma tarefa',
-                    hintText: 'Ex.: Comprar leite',
-                   
-                    border: OutlineInputBorder(),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'adicione uma tarefa',
+                        hintText: 'Ex.: Comprar leite',
+                       
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed:() {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5), // Define o raio do botão
+                      ),
+                      padding: EdgeInsets.all(16),
+                    ),
+                      child: Icon(
+                      Icons.add,
+                      size: 30,
+                      color: Colors.white,
+                      
+                      ),
+                    ),
+                    
+                    
+                  
+                ],
               ),
-              SizedBox(width: 8),
-              ElevatedButton(
-                onPressed:() {},
-                child: Text('+'),
+
+              SizedBox(height: 16),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                 ListTile(
+                    title: Text('Tarefa 1'),
+                    subtitle: Text('Descrição da tarefa 1'),
+                    leading: Icon(Icons.check_circle, color: Colors.green),
+                    onTap: (){
+                      print('Tarefa 1 clicada');
+                    },
+                 ),
+                 
+                ],
               ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'voce tem tarefas 0 pendentes',
+                      style: TextStyle(
+                        fontSize: 16,
+                        
+                      ),
+                     
+                    ),
+                  ),
+               
+
+                  
+
+                   SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('limpar tudo'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // Define a cor de fundo do botão
+                      foregroundColor: Colors.white, // Define a cor do texto do botão
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5), // Define o raio do botão
+                      ),
+                      padding: EdgeInsets.all(16),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
